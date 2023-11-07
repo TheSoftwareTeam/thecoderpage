a97f0f4af175adfff6e863b7ae81deb8c73986c1
muratokur3 <muratokur3@icloud.com>
2023-11-07 17:50:02 +0300

    master

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect } from "react";
 import "./profile.scss";
 import DataContext from "../../context/DataContext";
-import image from"../../images/muratokur.jpeg";
+import image from"../../images/avatar.png";
 const Profile = () => {
   const { state,dispatch,editProfile,handleImageChange } = useContext(DataContext);
-useEffect(()=>{
-  if(state.activeUser !== null){
-  dispatch({type:"profileName",payload:state.activeUser.name})
-  dispatch({type:"profileSurname",payload:state.activeUser.surName})
-  }
   
-},[])
+useEffect(()=>{
+
+  if(state.activeUser !== null){
+    dispatch({type:"profileName",payload: state.activeUser.name})
+    dispatch({type:"profileSurname",payload: state.activeUser.surName})
+    }
+  
+},[state.activeUser])
   return (
     <div className="profile-container">
       <h2>Profile</h2>
@@ -20,14 +22,15 @@ useEffect(()=>{
             src={image}
             alt="res"
           />
-          <input accept="images/*" type="file" onChange={handleImageChange}/>
         </div>
         <form onSubmit={editProfile}>
-          <input onChange={(e)=>dispatch({type:"profileName",payload:e.target.value})} type="text"  placeholder="Ad" value={state.profileName}/>
 
-          <input onChange={(e)=>dispatch({type:"profileSurname",payload:e.target.value})}  type="text" placeholder="Soyad" value={state.profileSurname}/>
-          <input type="text" value={state.activeUser.userName} readOnly/>
-          <input type="email" value={state.activeUser.email} readOnly/>
+          <input onChange={(e)=>dispatch({type:"profileName",payload:e.target.value})} type="text"  placeholder="Ad" value={state.profileName} required/>
+
+          <input onChange={(e)=>dispatch({type:"profileSurname",payload:e.target.value})}  type="text" placeholder="Soyad" value={state.profileSurname} required/>
+
+           <input type="text" value={state.activeUser!==null?state.activeUser.userName:""} readOnly/>
+          <input type="email" value={state.activeUser!==null?state.activeUser.email:""}  readOnly/>
           <input type="submit"  value="Edit Profile" />
         </form>
       </div>
