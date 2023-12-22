85fc5900bb0505dab6bc76a5c642cbaed0d07426
mahirkursun <mahirkrsn@gmail.com>
2023-12-22 14:16:31 +0300

    master

--- server/jsonServerApp.js
+++ server/jsonServerApp.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3005;
 
 server.use(middlewares);
-server.use('/api', router);
+server.use(router);
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
