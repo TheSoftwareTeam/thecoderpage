460df178896157a20ae05a29f14f6404fda54837
mahirkursun <mahirkrsn@gmail.com>
2023-12-22 12:45:26 +0300

    master

--- server/jsonServerApp.js
+++ server/jsonServerApp.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json'); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3005;
+const port = 3005;
 
 server.use(middlewares);
 server.use(router);
